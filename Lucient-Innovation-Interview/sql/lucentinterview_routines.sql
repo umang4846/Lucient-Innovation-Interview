fd34fbc53abb6bd1b03ea55144bdf516e64a825d
Umang Patel <umang4846@gmail.com>
2020-04-12 19:32:09 +0530

    Add files via upload

--- Lucient-Innovation-Interview/sql/lucentinterview_routines.sql
+++ Lucient-Innovation-Interview/sql/lucentinterview_routines.sql
@@ -16,14 +16,8 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `customers`
+-- Dumping events for database 'lucentinterview'
 --
-
-LOCK TABLES `customers` WRITE;
-/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
-INSERT INTO `customers` VALUES (1,'Umang Patel','Male','umang@gmail.com','98653214785','address 1','address 2','Gujarat','Ahmedabad','352552','India'),(4,'newly','Female','newly@gmail.com','253658958','address222','address3333','gujarat','baroda','253614','INDIA'),(10,'Vitthalbhai Patel','xsxx','gamasd','xsdsdd','mahesana','Modhera Road','mahesnas','ssd','384002','India'),(11,'Parth','Male','parth@gmail.com','852963741','hellosssss','wecclome address2','Delhi','New york','435678','US'),(12,'Umang','Male','Umang@gmail.com','09090909909','Address a ','Address b','AHMEDABAD','GUJARAT',NULL,'INDIA');
-/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -34,4 +28,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-12 19:26:40
+-- Dump completed on 2020-04-12 19:26:42
